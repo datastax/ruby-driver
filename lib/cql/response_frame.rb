c4ec9367d01db116f09d62176561de6684348f15
Theo <theo@iconara.net>
2013-01-18 15:22:29 +0100

    Extracted error responses with an error code to their own class

--- lib/cql/response_frame.rb
+++ lib/cql/response_frame.rb
@@ -137,47 +137,64 @@ module Cql
   end
 
   class ErrorResponse < ResponseBody
-    attr_reader :code, :message, :details
+    attr_reader :code, :message
 
     def initialize(*args)
-      @code, @message, @details = args
+      @code, @message = args
     end
 
     def self.decode!(buffer)
       code = read_int!(buffer)
       message = read_string!(buffer)
-      details = nil
+      case code
+      when 0x1000, 0x1100, 0x1200, 0x2400, 0x2500
+        DetailedErrorResponse.decode!(code, message, buffer)
+      else
+        new(code, message)
+      end
+    end
+
+    def to_s
+      %(ERROR #{code} "#{message}")
+    end
+  end
+
+  class DetailedErrorResponse < ErrorResponse
+    attr_reader :details
+
+    def initialize(code, message, details)
+      super(code, message)
+      @details = details
+    end
+
+    def self.decode!(code, message, buffer)
+      details = {}
       case code
       when 0x1000 # unavailable
-        details = {}
         details[:cl] = read_consistency!(buffer)
         details[:required] = read_int!(buffer)
         details[:alive] = read_int!(buffer)
       when 0x1100 # write_timeout
-        details = {}
         details[:cl] = read_consistency!(buffer)
         details[:received] = read_int!(buffer)
         details[:blockfor] = read_int!(buffer)
         details[:write_type] = read_string!(buffer)
       when 0x1200 # read_timeout
-        details = {}
         details[:cl] = read_consistency!(buffer)
         details[:received] = read_int!(buffer)
         details[:blockfor] = read_int!(buffer)
         details[:data_present] = read_byte!(buffer) != 0
       when 0x2400 # already_exists
-        details = {}
         details[:ks] = read_string!(buffer)
         details[:table] = read_string!(buffer)
       when 0x2500
-        details = {}
         details[:id] = read_short_bytes!(buffer)
       end
       new(code, message, details)
     end
 
     def to_s
-      %(ERROR #{code} "#{message}")
+      %(ERROR #{code} "#{message}" #{details.inspect})
     end
   end
 
