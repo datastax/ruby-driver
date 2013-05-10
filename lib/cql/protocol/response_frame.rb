5af30e4d27e128dfdbcc3886ed61f61c37f8c331
Theo <theo@iconara.net>
2013-05-10 16:01:31 +0200

    Optimized type lookup in the type conversion code

--- lib/cql/protocol/response_frame.rb
+++ lib/cql/protocol/response_frame.rb
@@ -364,87 +364,159 @@ module Cql
         end
       end
 
-      def self.convert_type(buffer, type, size_bytes=4)
-        return nil if buffer.empty?
-        case type
-        when :ascii
+      class TypeConverter
+        include Decoding
+
+        def initialize
+          @conversions = conversions
+        end
+
+        def convert_type(buffer, type, size_bytes=4)
+          return nil if buffer.empty?
+          case type
+          when Array
+            buffer.discard(size_bytes)
+            case type.first
+            when :list
+              convert_list(buffer, @conversions[type[1]])
+            when :map
+              convert_map(buffer, @conversions[type[1]], @conversions[type[2]])
+            when :set
+              convert_set(buffer, @conversions[type[1]])
+            end
+          else
+            @conversions[type].call(buffer, size_bytes)
+          end
+        end
+
+        def conversions
+          {
+            :ascii => method(:convert_ascii),
+            :bigint => method(:convert_bigint),
+            :blob => method(:convert_blob),
+            :boolean => method(:convert_boolean),
+            :counter => method(:convert_bigint),
+            :decimal => method(:convert_decimal),
+            :double => method(:convert_double),
+            :float => method(:convert_float),
+            :int => method(:convert_int),
+            :timestamp => method(:convert_timestamp),
+            :varchar => method(:convert_varchar),
+            :text => method(:convert_varchar),
+            :varint => method(:convert_varint),
+            :timeuuid => method(:convert_uuid),
+            :uuid => method(:convert_uuid),
+            :inet => method(:convert_inet),
+          }
+        end
+
+        def convert_ascii(buffer, size_bytes)
           bytes = size_bytes == 4 ? read_bytes!(buffer) : read_short_bytes!(buffer)
           bytes ? bytes.force_encoding(::Encoding::ASCII) : nil
-        when :bigint
+        end
+
+        def convert_bigint(buffer, size_bytes)
           buffer.discard(size_bytes)
           read_long!(buffer)
-        when :blob
+        end
+
+        def convert_blob(buffer, size_bytes)
           bytes = size_bytes == 4 ? read_bytes!(buffer) : read_short_bytes!(buffer)
           bytes ? bytes : nil
-        when :boolean
+        end
+
+        def convert_boolean(buffer, size_bytes)
           buffer.discard(size_bytes)
           buffer.read(1) == Constants::TRUE_BYTE
-        when :counter
+        end
+
+        def convert_counter(buffer, size_bytes)
           buffer.discard(size_bytes)
           read_long!(buffer)
-        when :decimal
+        end
+
+        def convert_decimal(buffer, size_bytes)
           read_decimal!(buffer, buffer.read_int)
-        when :double
+        end
+
+        def convert_double(buffer, size_bytes)
           buffer.discard(size_bytes)
           read_double!(buffer)
-        when :float
+        end
+
+        def convert_float(buffer, size_bytes)
           buffer.discard(size_bytes)
           read_float!(buffer)
-        when :int
+        end
+
+        def convert_int(buffer, size_bytes)
           buffer.discard(size_bytes)
           read_int!(buffer)
-        when :timestamp
+        end
+
+        def convert_timestamp(buffer, size_bytes)
           buffer.discard(size_bytes)
           timestamp = read_long!(buffer)
           Time.at(timestamp/1000.0)
-        when :varchar, :text
+        end
+
+        def convert_varchar(buffer, size_bytes)
           bytes = size_bytes == 4 ? read_bytes!(buffer) : read_short_bytes!(buffer)
           bytes ? bytes.force_encoding(::Encoding::UTF_8) : nil
-        when :varint
+        end
+
+        def convert_varint(buffer, size_bytes)
           read_varint!(buffer, buffer.read_int)
-        when :timeuuid, :uuid
+        end
+
+        def convert_uuid(buffer, size_bytes)
           buffer.discard(size_bytes)
           read_uuid!(buffer)
-        when :inet
+        end
+
+        def convert_inet(buffer, size_bytes)
           size = size_bytes == 4 ? buffer.read_int : buffer.read_short
           IPAddr.new_ntoh(buffer.read(size))
-        when Array
-          buffer.discard(size_bytes)
-          case type.first
-          when :list
-            list = []
-            size = read_short!(buffer)
-            size.times do
-              list << convert_type(buffer, type.last, 2)
-            end
-            list
-          when :map
-            map = {}
-            size = read_short!(buffer)
-            size.times do
-              key = convert_type(buffer, type[1], 2)
-              value = convert_type(buffer, type[2], 2)
-              map[key] = value
-            end
-            map
-          when :set
-            set = Set.new
-            size = read_short!(buffer)
-            size.times do
-              set << convert_type(buffer, type.last, 2)
-            end
-            set
+        end
+
+        def convert_list(buffer, value_converter)
+          list = []
+          size = buffer.read_short
+          size.times do
+            list << value_converter.call(buffer, 2)
           end
+          list
+        end
+
+        def convert_map(buffer, key_converter, value_converter)
+          map = {}
+          size = buffer.read_short
+          size.times do
+            key = key_converter.call(buffer, 2)
+            value = value_converter.call(buffer, 2)
+            map[key] = value
+          end
+          map
+        end
+
+        def convert_set(buffer, value_converter)
+          set = Set.new
+          size = buffer.read_short
+          size.times do
+            set << value_converter.call(buffer, 2)
+          end
+          set
         end
       end
 
       def self.read_rows!(buffer, column_specs)
+        type_converter = TypeConverter.new
         rows_count = read_int!(buffer)
         rows = []
         rows_count.times do |row_index|
           row = {}
           column_specs.each do |column_spec|
-            row[column_spec[2]] = convert_type(buffer, column_spec[3])
+            row[column_spec[2]] = type_converter.convert_type(buffer, column_spec[3])
           end
           rows << row
         end
