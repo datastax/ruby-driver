ec7fcc3828ad5b183c63f7a6142142408eaae671
Theo <theo@iconara.net>
2013-05-10 13:51:31 +0200

    ByteBuffer again caches its length

--- lib/cql/byte_buffer.rb
+++ lib/cql/byte_buffer.rb
@@ -2,35 +2,31 @@
 
 module Cql
   class ByteBuffer
-    attr_reader :length
-    alias_method :size, :length
-    alias_method :bytesize, :length
-
     def initialize(initial_bytes='')
-      @bytes = ''
+      @read_buffer = ''
+      @write_buffer = ''
       @offset = 0
       @length = 0
       append(initial_bytes) unless initial_bytes.empty?
     end
 
+    attr_reader :length
+    alias_method :size, :length
+    alias_method :bytesize, :length
+
     def empty?
-      @length == 0
+      length == 0
     end
 
     def append(bytes)
-      if @offset >= MAX_OFFSET
-        @bytes = '' << to_s
-        @offset = 0
-        @length = @bytes.bytesize
-      end
       bytes = bytes.to_s
       unless bytes.ascii_only?
         bytes = bytes.dup.force_encoding(::Encoding::BINARY)
       end
-      retag = @bytes.empty?
-      @bytes << bytes
+      retag = @write_buffer.empty?
+      @write_buffer << bytes
+      @write_buffer.force_encoding(::Encoding::BINARY) if retag
       @length += bytes.bytesize
-      @bytes.force_encoding(::Encoding::BINARY) if retag
       self
     end
     alias_method :<<, :append
@@ -44,34 +40,69 @@ module Cql
 
     def read(n)
       raise RangeError, "#{n} bytes required but only #{@length} available" if @length < n
-      s = @bytes[@offset, n]
-      discard(n)
-      s
+      if @offset >= @read_buffer.bytesize
+        swap_buffers
+        s = @read_buffer[@offset, n]
+        @offset += n
+        @length -= n
+        s
+      elsif @offset + n > @read_buffer.bytesize
+        s = @read_buffer[@offset, @read_buffer.bytesize - @offset]
+        n -= s.bytesize
+        swap_buffers
+        s << @read_buffer[@offset, n]
+        @offset += n
+        @length -= n
+        s
+      else
+        s = @read_buffer[@offset, n]
+        @offset += n
+        @length -= n
+        s
+      end
     end
 
     def read_int
       raise RangeError, "4 bytes required to read an int, but only #{@length} available" if @length < 4
-      i0 = @bytes.getbyte(@offset + 0)
-      i1 = @bytes.getbyte(@offset + 1)
-      i2 = @bytes.getbyte(@offset + 2)
-      i3 = @bytes.getbyte(@offset + 3)
-      discard(4)
+      if @read_buffer.bytesize >= @offset + 4
+        i0 = @read_buffer.getbyte(@offset + 0)
+        i1 = @read_buffer.getbyte(@offset + 1)
+        i2 = @read_buffer.getbyte(@offset + 2)
+        i3 = @read_buffer.getbyte(@offset + 3)
+        @offset += 4
+        @length -= 4
+      else
+        i0 = read_byte
+        i1 = read_byte
+        i2 = read_byte
+        i3 = read_byte
+      end
       (i0 << 24) | (i1 << 16) | (i2 << 8) | i3
     end
 
     def read_short
       raise RangeError, "2 bytes required to read a short, but only #{@length} available" if @length < 2
-      i0 = @bytes.getbyte(@offset + 0)
-      i1 = @bytes.getbyte(@offset + 1)
-      discard(2)
+      if @read_buffer.bytesize >= @offset + 2
+        i0 = @read_buffer.getbyte(@offset + 0)
+        i1 = @read_buffer.getbyte(@offset + 1)
+        @offset += 2
+        @length -= 2
+      else
+        i0 = read_byte
+        i1 = read_byte
+      end
       (i0 << 8) | i1
     end
 
     def read_byte(signed=false)
       raise RangeError, "No bytes available to read byte" if empty?
-      b = @bytes.getbyte(@offset)
+      if @offset >= @read_buffer.bytesize
+        swap_buffers
+      end
+      b = @read_buffer.getbyte(@offset)
       b = (b & 0x7f) - (b & 0x80) if signed
-      discard(1)
+      @offset += 1
+      @length -= 1
       b
     end
 
@@ -81,7 +112,7 @@ module Cql
     alias_method :==, :eql?
 
     def hash
-      @bytes.hash
+      bytes.hash
     end
 
     def dup
@@ -100,7 +131,7 @@ module Cql
     protected
 
     def bytes
-      @bytes[@offset, @length]
+      (@read_buffer + @write_buffer)[@offset, @length]
     end
 
     private
@@ -108,5 +139,11 @@ module Cql
     MAX_OFFSET = 2**20
     INT_FORMAT = 'N'.freeze
     SHORT_FORMAT = 'n'.freeze
+
+    def swap_buffers
+      @offset -= @read_buffer.bytesize
+      @read_buffer = @write_buffer
+      @write_buffer = ''
+    end
   end
 end
