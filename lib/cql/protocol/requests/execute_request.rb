0f6f6a7b9c7da7c0333ae9abe0bd88ec4315f7dd
Theo <theo@iconara.net>
2013-05-27 21:24:43 +0200

    Moved type encoding from ExecuteRequest to TypeConverter

--- lib/cql/protocol/requests/execute_request.rb
+++ lib/cql/protocol/requests/execute_request.rb
@@ -15,10 +15,11 @@ module Cql
       end
 
       def write(io)
+        type_converter = TypeConverter.new
         write_short_bytes(io, @id)
         write_short(io, @metadata.size)
         @metadata.each_with_index do |(_, _, _, type), index|
-          write_value(io, @values[index], type)
+          type_converter.to_bytes(io, type, @values[index])
         end
         write_consistency(io, @consistency)
       end
@@ -42,87 +43,6 @@ module Cql
           h = ((h & 33554431) * 31) ^ @consistency.hash
           h
         end
-      end
-
-      private
-
-      def write_value(io, value, type)
-        if Array === type
-          raise InvalidValueError, 'Value for collection must be enumerable' unless value.is_a?(Enumerable)
-          case type.first
-          when :list, :set
-            _, sub_type = type
-            raw = ''
-            write_short(raw, value.size)
-            value.each do |element|
-              rr = ''
-              write_value(rr, element, sub_type)
-              raw << rr[2, rr.length - 2]
-            end
-            write_bytes(io, raw)
-          when :map
-            _, key_type, value_type = type
-            raw = ''
-            write_short(raw, value.size)
-            value.each do |key, value|
-              rr = ''
-              write_value(rr, key, key_type)
-              raw << rr[2, rr.length - 2]
-              rr = ''
-              write_value(rr, value, value_type)
-              raw << rr[2, rr.length - 2]
-            end
-            write_bytes(io, raw)
-          else
-            raise UnsupportedColumnTypeError, %(Unsupported column collection type: #{type.first})
-          end
-        else
-          case type
-          when :ascii
-            write_bytes(io, value.encode(::Encoding::ASCII))
-          when :bigint
-            write_int(io, 8)
-            write_long(io, value)
-          when :blob
-            write_bytes(io, value.encode(::Encoding::BINARY))
-          when :boolean
-            write_int(io, 1)
-            io << (value ? Constants::TRUE_BYTE : Constants::FALSE_BYTE)
-          when :decimal
-            raw = write_decimal('', value)
-            write_int(io, raw.size)
-            io << raw
-          when :double
-            write_int(io, 8)
-            write_double(io, value)
-          when :float
-            write_int(io, 4)
-            write_float(io, value)
-          when :inet
-            write_int(io, value.ipv6? ? 16 : 4)
-            io << value.hton
-          when :int
-            write_int(io, 4)
-            write_int(io, value)
-          when :text, :varchar
-            write_bytes(io, value.encode(::Encoding::UTF_8))
-          when :timestamp
-            ms = (value.to_f * 1000).to_i
-            write_int(io, 8)
-            write_long(io, ms)
-          when :timeuuid, :uuid
-            write_int(io, 16)
-            write_uuid(io, value)
-          when :varint
-            raw = write_varint('', value)
-            write_int(io, raw.length)
-            io << raw
-          else
-            raise UnsupportedColumnTypeError, %(Unsupported column type: #{type})
-          end
-        end
-      rescue TypeError => e
-        raise TypeError, %("#{value}" cannot be encoded as #{type.to_s.upcase}: #{e.message}), e.backtrace
       end
     end
   end
--- lib/cql/protocol/type_converter.rb
+++ lib/cql/protocol/type_converter.rb
@@ -8,9 +8,11 @@ module Cql
   module Protocol
     class TypeConverter
       include Decoding
+      include Encoding
 
       def initialize
-        @conversions = conversions
+        @from_bytes_converters = from_bytes_converters
+        @to_bytes_converters = to_bytes_converters
       end
 
       def from_bytes(buffer, type, size_bytes=4)
@@ -20,18 +22,64 @@ module Cql
           return nil unless read_size(buffer, size_bytes)
           case type.first
           when :list
-            bytes_to_list(buffer, @conversions[type[1]])
+            bytes_to_list(buffer, @from_bytes_converters[type[1]])
           when :map
-            bytes_to_map(buffer, @conversions[type[1]], @conversions[type[2]])
+            bytes_to_map(buffer, @from_bytes_converters[type[1]], @from_bytes_converters[type[2]])
           when :set
-            bytes_to_set(buffer, @conversions[type[1]])
+            bytes_to_set(buffer, @from_bytes_converters[type[1]])
           end
         else
-          @conversions[type].call(buffer, size_bytes)
+          @from_bytes_converters[type].call(buffer, size_bytes)
         end
       end
 
-      def conversions
+      def to_bytes(io, type, value)
+        case type
+        when Array
+          unless value.is_a?(Enumerable)
+            raise InvalidValueError, 'Value for collection must be enumerable'
+          end
+          case type.first
+          when :list, :set
+            _, sub_type = type
+            raw = ''
+            write_short(raw, value.size)
+            value.each do |element|
+              rr = ''
+              to_bytes(rr, sub_type, element)
+              raw << rr[2, rr.length - 2]
+            end
+            write_bytes(io, raw)
+          when :map
+            _, key_type, value_type = type
+            raw = ''
+            write_short(raw, value.size)
+            value.each do |key, value|
+              rr = ''
+              to_bytes(rr, key_type, key)
+              raw << rr[2, rr.length - 2]
+              rr = ''
+              to_bytes(rr, value_type, value)
+              raw << rr[2, rr.length - 2]
+            end
+            write_bytes(io, raw)
+          else
+            raise UnsupportedColumnTypeError, %(Unsupported column collection type: #{type.first})
+          end
+        else
+          converter = @to_bytes_converters[type]
+          unless converter
+            raise UnsupportedColumnTypeError, %(Unsupported column type: #{type})
+          end
+          converter.call(io, value)
+        end
+      rescue TypeError => e
+        raise TypeError, %("#{value}" cannot be encoded as #{type.to_s.upcase}: #{e.message}), e.backtrace
+      end
+
+      private
+
+      def from_bytes_converters
         {
           :ascii => method(:bytes_to_ascii),
           :bigint => method(:bytes_to_bigint),
@@ -50,6 +98,37 @@ module Cql
           :uuid => method(:bytes_to_uuid),
           :inet => method(:bytes_to_inet),
         }
+      end
+
+      def to_bytes_converters
+        {
+          :ascii => method(:ascii_to_bytes),
+          :bigint => method(:bigint_to_bytes),
+          :blob => method(:blob_to_bytes),
+          :boolean => method(:boolean_to_bytes),
+          :decimal => method(:decimal_to_bytes),
+          :double => method(:double_to_bytes),
+          :float => method(:float_to_bytes),
+          :inet => method(:inet_to_bytes),
+          :int => method(:int_to_bytes),
+          :text => method(:varchar_to_bytes),
+          :varchar => method(:varchar_to_bytes),
+          :timestamp => method(:timestamp_to_bytes),
+          :timeuuid => method(:uuid_to_bytes),
+          :uuid => method(:uuid_to_bytes),
+          :varint => method(:varint_to_bytes),
+        }
+      end
+
+      def read_size(buffer, size_bytes)
+        if size_bytes == 2
+          size = buffer.read_short
+          return nil if size & 0x8000 == 0x8000
+        else
+          size = buffer.read_int
+          return nil if size & 0x80000000 == 0x80000000
+        end
+        size
       end
 
       def bytes_to_ascii(buffer, size_bytes)
@@ -150,15 +229,69 @@ module Cql
         set
       end
 
-      def read_size(buffer, size_bytes)
-        if size_bytes == 2
-          size = buffer.read_short
-          return nil if size & 0x8000 == 0x8000
-        else
-          size = buffer.read_int
-          return nil if size & 0x80000000 == 0x80000000
-        end
-        size
+      def ascii_to_bytes(io, value)
+        write_bytes(io, value.encode(::Encoding::ASCII))
+      end
+
+      def bigint_to_bytes(io, value)
+        write_int(io, 8)
+        write_long(io, value)
+      end
+
+      def blob_to_bytes(io, value)
+        write_bytes(io, value.encode(::Encoding::BINARY))
+      end
+
+      def boolean_to_bytes(io, value)
+        write_int(io, 1)
+        io << (value ? Constants::TRUE_BYTE : Constants::FALSE_BYTE)
+      end
+
+      def decimal_to_bytes(io, value)
+        raw = write_decimal('', value)
+        write_int(io, raw.size)
+        io << raw
+      end
+
+      def double_to_bytes(io, value)
+        write_int(io, 8)
+        write_double(io, value)
+      end
+
+      def float_to_bytes(io, value)
+        write_int(io, 4)
+        write_float(io, value)
+      end
+
+      def inet_to_bytes(io, value)
+        write_int(io, value.ipv6? ? 16 : 4)
+        io << value.hton
+      end
+
+      def int_to_bytes(io, value)
+        write_int(io, 4)
+        write_int(io, value)
+      end
+
+      def varchar_to_bytes(io, value)
+        write_bytes(io, value.encode(::Encoding::UTF_8))
+      end
+
+      def timestamp_to_bytes(io, value)
+        ms = (value.to_f * 1000).to_i
+        write_int(io, 8)
+        write_long(io, ms)
+      end
+
+      def uuid_to_bytes(io, value)
+        write_int(io, 16)
+        write_uuid(io, value)
+      end
+
+      def varint_to_bytes(io, value)
+        raw = write_varint('', value)
+        write_int(io, raw.length)
+        io << raw
       end
     end
   end
