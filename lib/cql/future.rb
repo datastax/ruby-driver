941331dc8fb3d2bf65d252905d52ce0b089c1801
Theo <theo@iconara.net>
2013-05-29 10:35:25 +0200

    Fixes to the Future API docs

--- lib/cql/future.rb
+++ lib/cql/future.rb
@@ -40,7 +40,7 @@ module Cql
 
     # Creates a new future which is failed.
     #
-    # @param [Error] the error of the created future
+    # @param [Error] error the error of the created future
     # @return [Future] a failed future
     #
     def self.failed(error)
@@ -91,7 +91,7 @@ module Cql
     #
     # If the future fails this method will raise the error that failed the future.
     #
-    # @returns [Object] the value of this future
+    # @return [Object] the value of this future
     #
     def value
       raise @error if @error
