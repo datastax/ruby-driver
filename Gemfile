7c865a9bfc71b6fa23aa5de55ee0f1cbca1f7b03
Theo <theo@iconara.net>
2013-07-04 09:19:18 +0200

    Testing Coveralls CI

--- Gemfile
+++ Gemfile
@@ -20,4 +20,5 @@ end
 group :test do
   gem 'rspec'
   gem 'simplecov'
+  gem 'coveralls', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ ENV['CASSANDRA_HOST'] ||= '127.0.0.1'
 require 'bundler/setup'
 
 unless ENV['COVERAGE'] == 'no'
+  require 'coveralls'
+  Coveralls.wear!
+
   require 'simplecov'
   SimpleCov.start do
     add_group 'Source', 'lib'
