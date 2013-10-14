8e9761f3acab02a80546263b857cb893dda6b8c9
Theo <theo@iconara.net>
2013-10-14 09:59:14 +0200

    Use Queue as a semaphore in Future#value
    It looks like the signalling with Mutex#sleep and Thread#wakeup has some kind of race, so use Queue instead.

--- lib/cql/future.rb
+++ lib/cql/future.rb
@@ -327,18 +327,21 @@ module Cql
     #
     # @return [Object] the value of this future
     def value
+      semaphore = nil
       @lock.synchronize do
         raise @error if @failed
         return @value if @resolved
-        t = Thread.current
-        u = proc { t.wakeup }
+        semaphore = Queue.new
+        u = proc { semaphore << :unblock }
         @value_listeners << u
         @failure_listeners << u
-        while true
+      end
+      while true
+        @lock.synchronize do
           raise @error if @failed
           return @value if @resolved
-          @lock.sleep(1)
         end
+        semaphore.pop
       end
     end
 
