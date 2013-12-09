iconara/cql-rb

Add support for request tracing
Pass :trace => true to #execute to request tracing, traces are automatically loaded for responses with the tracing flag set.

lib/cql/protocol/responses/event_response.rb
@@ -3,15 +3,15 @@
 module Cql
   module Protocol
     class EventResponse < ResultResponse
-      def self.decode!(buffer)
+      def self.decode!(buffer, trace_id=nil)
         type = read_string!(buffer)
         case type
         when SchemaChangeEventResponse::TYPE
-          SchemaChangeEventResponse.decode!(buffer)
+          SchemaChangeEventResponse.decode!(buffer, trace_id)
         when StatusChangeEventResponse::TYPE
-          StatusChangeEventResponse.decode!(buffer)
+          StatusChangeEventResponse.decode!(buffer, trace_id)
         when TopologyChangeEventResponse::TYPE
-          TopologyChangeEventResponse.decode!(buffer)
+          TopologyChangeEventResponse.decode!(buffer, trace_id)
         else
           raise UnsupportedEventTypeError, %(Unsupported event type: "#{type}")
         end

lib/cql/protocol/responses/result_response.rb
@@ -3,19 +3,25 @@
 module Cql
   module Protocol
     class ResultResponse < Response
-      def self.decode!(buffer)
+      attr_reader :trace_id
+
+      def initialize(trace_id)
+        @trace_id = trace_id
+      end
+
+      def self.decode!(buffer, trace_id=nil)
         kind = read_int!(buffer)
         case kind
         when 0x01
-          VoidResultResponse.decode!(buffer)
+          VoidResultResponse.decode!(buffer, trace_id)
         when 0x02
-          RowsResultResponse.decode!(buffer)
+          RowsResultResponse.decode!(buffer, trace_id)
         when 0x03
-          SetKeyspaceResultResponse.decode!(buffer)
+          SetKeyspaceResultResponse.decode!(buffer, trace_id)
         when 0x04
-          PreparedResultResponse.decode!(buffer)
+          PreparedResultResponse.decode!(buffer, trace_id)
         when 0x05
-          SchemaChangeResultResponse.decode!(buffer)
+          SchemaChangeResultResponse.decode!(buffer, trace_id)
         else
           raise UnsupportedResultKindError, %(Unsupported result kind: #{kind})
         end

datastax/ruby-driver

lib/cql/protocol/responses/event_response.rb
@@ -3,11 +3,11 @@
 module Cql
   module Protocol
     class EventResponse < ResultResponse
-      def self.decode!(buffer)
+      def self.decode!(buffer, trace_id=nil)
         type = read_string!(buffer)
         impl = EVENT_TYPES[type]
         raise UnsupportedEventTypeError, %(Unsupported event type: "#{type}") unless impl
-        impl.decode!(buffer)
+        impl.decode!(buffer, trace_id)
       end
 
       private

lib/cql/protocol/responses/result_response.rb
@@ -3,11 +3,17 @@
 module Cql
   module Protocol
     class ResultResponse < Response
-      def self.decode!(buffer)
+      attr_reader :trace_id
+
+      def initialize(trace_id)
+        @trace_id = trace_id
+      end
+
+      def self.decode!(buffer, trace_id=nil)
         kind = read_int!(buffer)
         impl = RESULT_TYPES[kind]
         raise UnsupportedResultKindError, %(Unsupported result kind: #{kind}) unless impl
-        impl.decode!(buffer)
+        impl.decode!(buffer, trace_id)
       end
 
       def void?