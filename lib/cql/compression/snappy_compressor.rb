6cda1cdc9cf33f181b5a6031631521f16ccd0856
Theo <theo@iconara.net>
2013-12-14 21:36:32 +0100

    Move the SnappyCompressor class to after the dependency check
    This way Yard discovers the class and it can be properly documented

--- lib/cql/compression/snappy_compressor.rb
+++ lib/cql/compression/snappy_compressor.rb
@@ -1,32 +1,33 @@
 # encoding: utf-8
 
+begin
+  require 'snappy'
+rescue LoadError => e
+  raise LoadError, %[Snappy support requires the "snappy" gem: #{e.message}], e.backtrace
+end
+
 module Cql
   module Compression
-    begin
-      require 'snappy'
 
-      class SnappyCompressor
-        attr_reader :algorithm
+    class SnappyCompressor
+      attr_reader :algorithm
 
-        def initialize(min_size=64)
-          @algorithm = 'snappy'.freeze
-          @min_size = min_size
-        end
-
-        def compress?(str)
-          str.bytesize > @min_size
-        end
-
-        def compress(str)
-          Snappy.deflate(str)
-        end
-
-        def decompress(str)
-          Snappy.inflate(str)
-        end
+      def initialize(min_size=64)
+        @algorithm = 'snappy'.freeze
+        @min_size = min_size
       end
-    rescue LoadError => e
-      raise LoadError, %[Snappy support requires the "snappy" gem: #{e.message}], e.backtrace
+
+      def compress?(str)
+        str.bytesize > @min_size
+      end
+
+      def compress(str)
+        Snappy.deflate(str)
+      end
+
+      def decompress(str)
+        Snappy.inflate(str)
+      end
     end
   end
 end
