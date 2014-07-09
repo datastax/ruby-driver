caa0f686425c64aaddb2e1df2ad9b537aa4c1e8c
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2014-07-08 22:56:03 -0700

    add status field to host

--- lib/cql/host.rb
+++ lib/cql/host.rb
@@ -2,14 +2,15 @@
 
 module Cql
   class Host
-    attr_reader :ip, :id, :rack, :datacenter, :release_version
+    attr_reader :ip, :id, :rack, :datacenter, :release_version, :status
 
-    def initialize(ip, id, rack, datacenter, release_version)
+    def initialize(ip, id, rack, datacenter, release_version, status)
       @ip              = ip
       @id              = id
       @rack            = rack
       @datacenter      = datacenter
       @release_version = release_version
+      @status          = status
     end
   end
 end
