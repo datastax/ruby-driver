79f8a8ff36becfb9f715aa598c1d3a105c8dab9d
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2014-07-09 16:02:48 -0700

    add reconnect interval option

--- lib/cql/builder/settings.rb
+++ lib/cql/builder/settings.rb
@@ -6,11 +6,11 @@ module Cql
       attr_reader   :addresses
       attr_accessor :port, :protocol_version, :connection_timeout,
                     :default_consistency, :logger, :compressor, :credentials,
-                    :auth_provider
+                    :auth_provider, :reconnect_interval
 
       def initialize(addresses, port, protocol_version, connection_timeout,
                      default_consistency, logger, compressor, credentials,
-                     auth_provider)
+                     auth_provider, reconnect_interval)
         @addresses           = addresses
         @port                = port
         @protocol_version    = protocol_version
@@ -20,6 +20,7 @@ module Cql
         @compressor          = compressor
         @credentials         = credentials
         @auth_provider       = auth_provider
+        @reconnect_interval  = reconnect_interval
       end
     end
   end
