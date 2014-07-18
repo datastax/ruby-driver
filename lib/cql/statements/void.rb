d6130144863c88612f973f684c9d1d12631f4eea
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2014-07-18 00:45:26 -0700

    clarify statement interface

--- lib/cql/statements/void.rb
+++ lib/cql/statements/void.rb
@@ -3,6 +3,15 @@
 module Cql
   module Statements
     class Void
+      @@params = [].freeze
+
+      def cql
+        nil
+      end
+
+      def params
+        @@params
+      end
     end
   end
 end
