bd298a24bb468c54936921be7c35790d8c755564
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2014-07-18 11:52:39 -0700

    remove unnecessary methods

--- lib/cql/statements/void.rb
+++ lib/cql/statements/void.rb
@@ -3,15 +3,6 @@
 module Cql
   module Statements
     class Void
-      @@params = [].freeze
-
-      def cql
-        nil
-      end
-
-      def params
-        @@params
-      end
     end
   end
 end
