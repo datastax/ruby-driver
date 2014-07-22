273ad08098dedad0fa4e65c3991e0cb51fb382ca
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2014-07-21 17:53:48 -0700

    check consistency level in dc aware policy

--- lib/cql/load_balancing/policies/dc_aware_round_robin.rb
+++ lib/cql/load_balancing/policies/dc_aware_round_robin.rb
@@ -35,25 +35,27 @@ module Cql
 
         include Policy
 
-        def initialize(datacenter, max_remote_hosts_to_use = 0)
+        def initialize(datacenter, max_remote_hosts_to_use = nil, use_remote_hosts_for_local_consistency = false)
           datacenter              = String(datacenter)
           max_remote_hosts_to_use = Integer(max_remote_hosts_to_use)
 
           raise ::ArgumentError, "datacenter cannot be nil" if datacenter.nil?
-          raise ::ArgumentError, "max_remote_hosts_to_use must be >= 0" if max_remote_hosts_to_use < 0
+          raise ::ArgumentError, "max_remote_hosts_to_use must be nil or >= 0" if !max_remote_hosts_to_use.nil? && max_remote_hosts_to_use < 0
 
           @datacenter = datacenter
           @max_remote = max_remote_hosts_to_use
           @local      = ::Set.new
           @remote     = ::Set.new
           @position   = 0
+
+          @use_remote = !!use_remote_hosts_for_local_consistency
         end
 
         def host_up(host)
           if host.datacenter.nil? || host.datacenter == @datacenter
             @local  = @local.dup.add(host)
           else
-            @remote = @remote.dup.add(host) if @remote.size < @max_remote
+            @remote = @remote.dup.add(host) if @max_remote && @remote.size < @max_remote
           end
 
           self
@@ -93,10 +95,21 @@ module Cql
 
           return EMPTY_PLAN if total == 0
 
+          if LOCAL_CONSISTENCIES.include?(options.consistency) && @use_remote
+            remote = remote.to_a
+          else
+            remote = EMPTY_ARRAY
+          end
+
           @position = (@position + 1) % total
 
-          Plan.new(local.to_a, remote.to_a, position)
+          Plan.new(local.to_a, remote, position)
         end
+
+        private
+
+        LOCAL_CONSISTENCIES = [:local_quorum, :local_one].freeze
+        EMPTY_ARRAY         = [].freeze
       end
     end
   end
