2b9e3dcacfebc5232ee05c142fccf80a82e318a2
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2014-07-21 18:27:03 -0700

    add downgrading consistency implementation

--- lib/cql/retry/policies.rb
+++ lib/cql/retry/policies.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
 
 require 'cql/retry/policies/default'
+require 'cql/retry/policies/downgrading_consistency'
 require 'cql/retry/policies/fallthrough'
---
+++ lib/cql/retry/policies/downgrading_consistency.rb
@@ -0,0 +1,58 @@
+# encoding: utf-8
+
+module Cql
+  module Retry
+    module Policies
+      class DowngradingConsistency
+        include Policy
+
+        def read_timeout(statement, consistency_level, required_responses,
+                         received_responses, data_retrieved, attempt)
+          return reraise if attempt > 1 || SERIAL_CONSISTENCIES.include?(consistency_level)
+          return max_likely_to_work(consistency_level, required_responses, received_responses) if received_responses < required_responses
+
+          data_retrieved ? reraise : try_again(consistency_level)
+        end
+
+        def write_timeout(statement, consistency_level, write_type,
+                          acks_required, acks_received, attempt)
+          return reraise if attempt > 1
+
+          case write_type
+          when 'SIMPLE', 'BATCH'
+            ignore
+          when 'UNLOGGED_BATCH'
+            max_likely_to_work(consistency_level, acks_required, acks_received)
+          when 'BATCH_LOG'
+            try_again(consistency_level)
+          else
+            reraise
+          end
+        end
+
+        def unavailable(statement, consistency_level, replicas_required,
+                        replicas_alive, attempt)
+          return reraise if attempt > 1
+
+          max_likely_to_work(consistency_level, replicas_required, replicas_alive)
+        end
+
+        private
+
+        def max_likely_to_work(consistency_level, required, received)
+          if consistency_level == :all && required > 1 && received >= (required.to_f / 2).floor + 1
+            try_again(:quorum)
+          elsif received >= 3
+            try_again(:three)
+          elsif received >= 2
+            try_again(:two)
+          elsif received >= 1
+            try_again(:one)
+          else
+            reraise
+          end
+        end
+      end
+    end
+  end
+end
