ac169746fca45b9653552f35b332c813e875f7f1
Pierre Laporte <pierre@pingtimeout.fr>
2014-07-25 15:44:01 +0200

    Left-padding iteration with zeros

--- benchmarking/run-benchmark.sh
+++ benchmarking/run-benchmark.sh
@@ -6,16 +6,17 @@ queries=$((1000 * 100))
 
 for i in `seq $iterations`
 do
-    ruby -rbundler/setup benchmark-ruby-driver.rb $queries > $current_dir/benchmark-ruby-driver-$i.log &
+    n=`printf '%02d' $i`
+    ruby -rbundler/setup benchmark-ruby-driver.rb $queries > $current_dir/benchmark-ruby-driver-$n.log &
     ruby_pid=$!
-    top -c d -stats command,pid,cpu,threads,csw -pid $ruby_pid -l 0 > $current_dir/top-ruby-driver-$i.log &
+    top -c d -stats command,pid,cpu,threads,csw -pid $ruby_pid -l 0 > $current_dir/top-ruby-driver-$n.log &
     top_pid=$!
     wait $ruby_pid
     kill $top_pid
 
-    ruby -rbundler/setup benchmark-cql-rb.rb $queries > $current_dir/benchmark-cql-rb-$i.log &
+    ruby -rbundler/setup benchmark-cql-rb.rb $queries > $current_dir/benchmark-cql-rb-$n.log &
     ruby_pid=$!
-    top -c d -stats command,pid,cpu,threads,csw -pid $ruby_pid -l 0 > $current_dir/top-cql-rb-$i.log &
+    top -c d -stats command,pid,cpu,threads,csw -pid $ruby_pid -l 0 > $current_dir/top-cql-rb-$n.log &
     top_pid=$!
     wait $ruby_pid
     kill $top_pid
