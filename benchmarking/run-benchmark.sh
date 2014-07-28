c0a848016dd246d421b435b45a5d79c5c2b467bf
Pierre Laporte <pierre@pingtimeout.fr>
2014-07-28 16:34:12 +0200

    Output files are in dedicated directory (current date)

--- benchmarking/run-benchmark.sh
+++ benchmarking/run-benchmark.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-current_dir=`dirname $0`
-
 # Script defaults : 10 measurements of 10k unprepared selects
 iterations=10
 queries=$((1000 * 10))
@@ -9,7 +7,7 @@ preparation="unprepared"
 operation="select"
 help=false
 
-while getopts "upsiln:" opt; do
+while getopts "hupsiln:" opt; do
     case $opt in
         u) preparation="unprepared" ;;
         p) preparation="prepared" ;;
@@ -27,11 +25,11 @@ if [ $help = true ] ; then
     echo "Usage: $0 [option...]
     Options:
     -h      Print this help
-    -n <n>  Execute <n> queries in each run
+    -n <n>  Execute <n> queries in each run (default 10.000)
     -l      Equivalent to -n 100000
-    -u      Execute only unprepared statements
+    -u      Execute only unprepared statements (default)
     -p      Execute only prepared statements
-    -s      Execute only select statements
+    -s      Execute only select statements (default)
     -i      Execute only insert statements" \
         >&2
     exit
@@ -41,19 +39,22 @@ echo "# Will run $iterations times $queries queries from :"
 echo "# * $preparation-$operation-cql-rb.rb"
 echo "# * $preparation-$operation-ruby-driver.rb"
 
+output_dir=`dirname $0`/`date '+%Y-%m-%dT%H:%M'`
+mkdir $output_dir
+
 for i in `seq $iterations`
 do
     n=`printf '%02d' $i`
-    ruby -rbundler/setup $preparation-$operation-cql-rb.rb $queries > $current_dir/output-$preparation-$operation-cql-rb-$n.log &
+    ruby -rbundler/setup $preparation-$operation-cql-rb.rb $queries > $output_dir/output-$preparation-$operation-cql-rb-$n.log &
     ruby_pid=$!
-    top -c d -stats command,pid,cpu,threads,csw -pid $ruby_pid -l 0 > $current_dir/top-$preparation-$operation-cql-rb-$n.log &
+    top -c d -stats command,pid,cpu,threads,csw -pid $ruby_pid -l 0 > $output_dir/top-$preparation-$operation-cql-rb-$n.log &
     top_pid=$!
     wait $ruby_pid
     kill $top_pid
 
-    ruby -rbundler/setup $preparation-$operation-ruby-driver.rb $queries > $current_dir/output-$preparation-$operation-ruby-driver-$n.log &
+    ruby -rbundler/setup $preparation-$operation-ruby-driver.rb $queries > $output_dir/output-$preparation-$operation-ruby-driver-$n.log &
     ruby_pid=$!
-    top -c d -stats command,pid,cpu,threads,csw -pid $ruby_pid -l 0 > $current_dir/top-$preparation-$operation-ruby-driver-$n.log &
+    top -c d -stats command,pid,cpu,threads,csw -pid $ruby_pid -l 0 > $output_dir/top-$preparation-$operation-ruby-driver-$n.log &
     top_pid=$!
     wait $ruby_pid
     kill $top_pid
