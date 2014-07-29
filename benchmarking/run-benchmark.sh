fd9edba2f8e50e0d00e2aa43e06f2855ca011146
Pierre Laporte <pierre@pingtimeout.fr>
2014-07-30 00:44:36 +0200

    Refactored run script to explicitely pass benchs to run
    Instead of having a combinatronics of all possible scenarios, the run
    script can be used with a vararg of tests (ruby files).  So it is now
    possible to run only benchmarks of ruby-driver for prepared and
    unprepared selects, without running cql-rb benchmarks at all.

--- benchmarking/run-benchmark.sh
+++ benchmarking/run-benchmark.sh
@@ -3,16 +3,10 @@
 # Script defaults : 10 measurements of 10k unprepared selects
 iterations=10
 queries=$((1000 * 10))
-preparation="unprepared"
-operation="select"
 help=false
 
-while getopts "hupsiln:" opt; do
+while getopts "hln:" opt; do
     case $opt in
-        u) preparation="unprepared" ;;
-        p) preparation="prepared" ;;
-        s) operation="select" ;;
-        i) operation="insert" ;;
         l) queries=$((1000 * 100)) ;;
         n) queries=$OPTARG ;;
         :) echo "Option -$OPTARG requires an argument." >&2 ; help=true ;;
@@ -22,41 +16,35 @@ done
 
 # Print help if asked
 if [ $help = true ] ; then
-    echo "Usage: $0 [option...]
+    echo "Usage: $0 [option...] file...
     Options:
     -h      Print this help
     -n <n>  Execute <n> queries in each run (default 10.000)
     -l      Equivalent to -n 100000
-    -u      Execute only unprepared statements (default)
-    -p      Execute only prepared statements
-    -s      Execute only select statements (default)
-    -i      Execute only insert statements" \
+
+    Arguments:
+    The list of files (ruby benchmarks) to execute" \
         >&2
     exit
 fi
 
-echo "# Will run $iterations times $queries queries from :"
-echo "# * $preparation-$operation-cql-rb.rb"
-echo "# * $preparation-$operation-ruby-driver.rb"
+echo "# Will run $iterations times $queries queries from : $@"
 
 output_dir=`dirname $0`/`date '+%Y-%m-%dT%H:%M'`
 mkdir $output_dir
 
-for i in `seq $iterations`
+for iteration in `seq $iterations`
 do
-    n=`printf '%02d' $i`
-    ruby -rbundler/setup $preparation-$operation-cql-rb.rb $queries > $output_dir/output-$preparation-$operation-cql-rb-$n.log &
-    ruby_pid=$!
-    top -c d -stats command,pid,cpu,threads,csw -pid $ruby_pid -l 0 > $output_dir/top-$preparation-$operation-cql-rb-$n.log &
-    top_pid=$!
-    wait $ruby_pid
-    kill $top_pid
-
-    ruby -rbundler/setup $preparation-$operation-ruby-driver.rb $queries > $output_dir/output-$preparation-$operation-ruby-driver-$n.log &
-    ruby_pid=$!
-    top -c d -stats command,pid,cpu,threads,csw -pid $ruby_pid -l 0 > $output_dir/top-$preparation-$operation-ruby-driver-$n.log &
-    top_pid=$!
-    wait $ruby_pid
-    kill $top_pid
+    for file in $@
+    do
+        test_name=${file%.*}
+        n=`printf '%02d' $iteration`
+        ruby -rbundler/setup $file $queries > $output_dir/output-$test_name-$n.log &
+        ruby_pid=$!
+        top -c d -stats command,pid,cpu,threads,csw -pid $ruby_pid -l 0 > $output_dir/top-$test_name-$n.log &
+        top_pid=$!
+        wait $ruby_pid
+        kill $top_pid
+    done
 done
 
