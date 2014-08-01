ab6d20cd56e4802127b8ba7997c6a7f16a508f26
Pierre Laporte <pierre@pingtimeout.fr>
2014-08-01 16:48:09 +0200

    Added options for cluster creation

--- benchmarking/create-cluster.sh
+++ benchmarking/create-cluster.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
 
-ccm create -n 3 -v git:cassandra-2.0.9 -i 127.0.0. -s -b test-cluster
+create=false
+insert=false
+help=false
 
-source ~/env/opt/apache-cassandra-2.0.9/env*.source
-echo "CREATE KEYSPACE simplex WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 3};" | cqlsh
-cqlsh -k simplex -f ../features/support/cql/schema/songs.cql
-cqlsh -k simplex -f ../features/support/cql/data/songs.cql
+
+
+while getopts "hci" opt; do
+    case $opt in
+        c) create=true ;;
+        i) insert=true ;;
+        :) echo "Option -$OPTARG requires an argument." >&2 ; help=true ;;
+        *) help=true ;; # Wrong argument == print help
+    esac
+done
+
+
+
+# Print help if asked
+if [ $help = true ] ; then
+    echo "Usage: $0 [option...]
+    Options:
+    -c      Create the cluster
+    -i      Insert data in the cluster (including create keyspace)" \
+        >&2
+    exit
+fi
+shift $((OPTIND-1))
+
+
+
+if [ $create = true ] ; then
+    ccm create -n 3 -v git:cassandra-2.0.9 -i 127.0.0. -s -b test-cluster
+fi
+
+
+
+if [ $insert = true ] ; then
+    echo "CREATE KEYSPACE simplex WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 3};" | cqlsh
+    cqlsh -k simplex -f ../features/support/cql/schema/songs.cql
+    cqlsh -k simplex -f ../features/support/cql/data/songs.cql
+fi
 
