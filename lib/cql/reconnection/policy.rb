547ff71587d317158a9b6a4556fe0c65e329bfa7
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2014-08-19 16:08:10 -0700

    document reconnection policy

--- lib/cql/reconnection/policy.rb
+++ lib/cql/reconnection/policy.rb
@@ -2,9 +2,21 @@
 
 module Cql
   module Reconnection
+    # @!parse [ruby]
+    #   class Schedule
+    #     # @return [Numeric] the next reconnection interval in seconds
+    #     def next
+    #     end
+    #   end
+
     module Policy
+      # Returns a reconnection schedule
+      #
+      # @abstract implementation should be provided by an actual policy
+      # @note reconnection schedule doesn't need to extend
+      #   {Cql::Reconnection::Schedule}, only conform to its interface
+      # @return [Cql::Reconnection::Schedule] reconnection schedule
       def schedule
-        raise ::NotImplementedError, "must be implemented by a child"
       end
     end
   end
