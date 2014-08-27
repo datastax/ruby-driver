abb573bb1a2653a0dc01dc749893b951c4f6ceef
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2014-08-26 17:27:55 -0700

    wake up waiters after notifying listeners

--- lib/cassandra/future.rb
+++ lib/cassandra/future.rb
@@ -474,12 +474,14 @@ module Cassandra
           @state = :broken
 
           listeners, @listeners = @listeners, nil
-
-          @cond.broadcast if @waiting > 0
         end
 
         listeners.each do |listener|
           listener.failure(error) rescue nil
+        end
+
+        synchronize do
+          @cond.broadcast if @waiting > 0
         end
 
         self
@@ -497,12 +499,14 @@ module Cassandra
           @state = :fulfilled
 
           listeners, @listeners = @listeners, nil
-
-          @cond.broadcast if @waiting > 0
         end
 
         listeners.each do |listener|
           listener.success(value) rescue nil
+        end
+
+        synchronize do
+          @cond.broadcast if @waiting > 0
         end
 
         self
