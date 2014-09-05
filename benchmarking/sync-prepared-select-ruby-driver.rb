025d77885a3825b08584d3cd5f282e7ecbc328b1
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2014-09-05 14:52:17 -0700

    add nodoc around license header

--- benchmarking/sync-prepared-select-ruby-driver.rb
+++ benchmarking/sync-prepared-select-ruby-driver.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+#--
 # Copyright 2013-2014 DataStax, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- lib/cassandra/load_balancing/policies.rb
+++ lib/cassandra/load_balancing/policies.rb
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#++
 
 require 'cassandra/load_balancing/policies/round_robin'
 require 'cassandra/load_balancing/policies/dc_aware_round_robin'
