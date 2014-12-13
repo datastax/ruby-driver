03f3656551c2f410a3cf373929f46ab2e88a92a6
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2014-12-12 17:35:23 -0800

    [ci skip] add copy code examples button

--- nanoc/assets/js/app.js
+++ nanoc/assets/js/app.js
@@ -1,9 +1,5 @@
 !(function(ng) {
-  var app = ng.module('docs', ['cfp.hotkeys'])
-
-  app.value('pages', <%= pages_json %>)
-
-  app.factory('basePath', function() {
+  function basePath() {
     var regexp = new RegExp('<%= item.path[1..-1] %>');
     var script = $('script').filter(function(i, el) {
       return el.src.match(regexp);
@@ -12,7 +8,13 @@
     var base = script.src.substr(window.location.protocol.length + window.location.host.length + 2, script.src.length);
 
     return base.replace('<%= item.path %>', '');
-  })
+  }
+
+  var app = ng.module('docs', ['cfp.hotkeys'])
+
+  app.value('pages', <%= pages_json %>)
+
+  app.factory('basePath', basePath)
 
   app.provider('search', function() {
     function localSearchFactory($http, $timeout, $q) {
@@ -155,13 +157,54 @@
       }
     }
   ])
-})(angular)
 
-$(function() {
-  $('#content').height(
-    Math.max(
-      $(".side-nav").height(),
-      $('#content').height()
-    )
-  );
-});
+
+  $(function() {
+    $('#content').height(
+      Math.max(
+        $(".side-nav").height(),
+        $('#content').height()
+      )
+    );
+
+    // Config ZeroClipboard
+    ZeroClipboard.config({
+      swfPath: basePath() + '/flash/ZeroClipboard.swf',
+      hoverClass: 'btn-clipboard-hover',
+      activeClass: 'btn-clipboard-active'
+    })
+
+    // Insert copy to clipboard button before .highlight
+    $('.highlight').each(function () {
+      var btnHtml = '<div class="zero-clipboard"><span class="btn-clipboard">Copy</span></div>'
+      $(this).before(btnHtml)
+    })
+
+    var zeroClipboard = new ZeroClipboard($('.btn-clipboard'))
+
+    // Handlers for ZeroClipboard
+
+    // Copy to clipboard
+    zeroClipboard.on('copy', function (event) {
+      var clipboard = event.clipboardData;
+      var highlight = $(event.target).parent().nextAll('.highlight').first()
+      clipboard.setData('text/plain', highlight.text())
+    })
+
+    // Notify copy success and reset tooltip title
+    zeroClipboard.on('aftercopy', function (event) {
+      $(event.target)
+        .attr('title', 'Copied!')
+        .tooltip('fixTitle')
+        .tooltip('show')
+    })
+
+    // Notify copy failure
+    zeroClipboard.on('error', function (event) {
+      $(event.target)
+        .attr('title', 'Flash required')
+        .tooltip('fixTitle')
+        .tooltip('show')
+    })
+  });
+})(angular)
