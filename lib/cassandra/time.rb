4ebf14ac40a5b7d4c53317edc0b1b054be0c49a6
Bulat Shakirzyanov <bulat.shakirzyanov@datastax.com>
2015-10-30 13:40:55 -0700

    [RUBY-115] add Cassandra::Time implementation

---
+++ lib/cassandra/time.rb
@@ -0,0 +1,61 @@
+# encoding: utf-8
+
+#--
+# Copyright 2013-2015 DataStax, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#++
+
+module Cassandra
+  # Represents a Cassandra time type.
+  class Time
+    NANOSECONDS_IN_MILISECOND = 1_000_000
+    NANOSECONDS_IN_SECOND     = NANOSECONDS_IN_MILISECOND * 1000
+    NANOSECONDS_IN_MINUTE     = NANOSECONDS_IN_SECOND * 60
+    NANOSECONDS_IN_HOUR       = NANOSECONDS_IN_MINUTE * 60
+    NANOSECONDS_IN_DAY        = NANOSECONDS_IN_HOUR * 24
+
+    def initialize(nanoseconds = 0)
+      if nanoseconds < 0 && nanoseconds > NANOSECONDS_IN_DAY - 1
+        raise ::ArgumentError, "value must be between 0 and " \
+                               "#{NANOSECONDS_IN_DAY}, #{value.inspect} given"
+      end
+
+      @nanoseconds = nanoseconds
+    end
+
+    def hours
+      @nanoseconds / NANOSECONDS_IN_HOUR
+    end
+
+    def minutes
+      (@nanoseconds - (hours * NANOSECONDS_IN_HOUR)) / NANOSECONDS_IN_MINUTE
+    end
+
+    def seconds
+      (@nanoseconds - (hours * NANOSECONDS_IN_HOUR) - (minutes * NANOSECONDS_IN_MINUTE)) / NANOSECONDS_IN_SECOND
+    end
+
+    def miliseconds
+      (@nanoseconds - (hours * NANOSECONDS_IN_HOUR) - (minutes * NANOSECONDS_IN_MINUTE) - (seconds * NANOSECONDS_IN_SECOND)) / NANOSECONDS_IN_MILISECOND
+    end
+
+    def to_s
+      '%.2d:%.2d:%.2d.%.3d' % [hours, minutes, seconds, miliseconds]
+    end
+
+    def to_nanoseconds
+      @nanoseconds
+    end
+  end
+end
