a2423415d331b013e7473005d591f139913075df
Sandeep Tamhankar <stamhankar@gmail.com>
2017-05-11 18:17:47 -0700

    Update Copyright notices on all files.

--- spec/cassandra/protocol/requests/credentials_request_spec.rb
+++ spec/cassandra/protocol/requests/credentials_request_spec.rb
@@ -1,7 +1,7 @@
 # encoding: ascii-8bit
 
 #--
-# Copyright 2013-2016 DataStax, Inc.
+# Copyright 2013-2017 DataStax, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
