cc2fc70bc5076cd759edd938b86cb1747835e8c8
Sandeep Tamhankar <stamhankar@gmail.com>
2018-02-20 13:05:22 -0800

    RUBY-322 - Decimals with zero scale aren't parsed properly.

--- spec/cassandra/protocol/cql_byte_buffer_spec.rb
+++ spec/cassandra/protocol/cql_byte_buffer_spec.rb
@@ -112,6 +112,31 @@ module Cassandra
           buffer.read_decimal.should == BigDecimal.new('-30000000')
         end
 
+        it 'decodes a decimal with zero scale' do
+          buffer = described_class.new("\x00\x00\x00\x00\x0a")
+          buffer.read_decimal.should == BigDecimal.new('10')
+        end
+
+        it 'decodes a negative decimal with zero scale' do
+          buffer = described_class.new("\x00\x00\x00\x00\xfe")
+          buffer.read_decimal.should == BigDecimal.new('-2')
+        end
+
+        it 'decodes zero decimal with zero scale' do
+          buffer = described_class.new("\x00\x00\x00\x00\x00")
+          buffer.read_decimal.should == BigDecimal.new('0')
+        end
+
+        it 'decodes zero decimal with negative scale' do
+          buffer = described_class.new("\xff\xff\xff\xf9\x00")
+          buffer.read_decimal.should == BigDecimal.new('0')
+        end
+
+        it 'decodes zero decimal with positive scale' do
+          buffer = described_class.new("\x00\x00\x00\x05\x00")
+          buffer.read_decimal.should == BigDecimal.new('0')
+        end
+
         it 'consumes the bytes' do
           buffer << 'HELLO'
           buffer.read_decimal(buffer.length - 5)
